2cd0d9d34fe4993dce62b7973d84f1a2f6b93ab8
Shamilxmhd <shamilxmuhd@gmail.com>
2024-03-12 23:16:14 +0530

    2nd

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -8,11 +8,13 @@ import {  ToastrService } from 'ngx-toastr';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
+
   checkoutStatus: boolean = false
+
   checkoutForm = this.fb.group({
-    name: ['', [Validators.pattern('[a-zA-Z]*'), Validators.required]],
-    address: ['', [Validators.pattern('[a-zA-Z0-9]*'), Validators.required]],
-    pincode: ['', [Validators.pattern('[0-9]*'), Validators.required]]
+    name: ['', [Validators.pattern('[a-zA-Z ]*'), Validators.required]],
+    address: ['', [Validators.pattern('[a-zA-Z0-9 ]*'), Validators.required]],
+    pincode: ['', [Validators.pattern('[0-9 ]*'), Validators.required]]
   })
 
   constructor(private fb: FormBuilder, private toaster: ToastrService) { }
